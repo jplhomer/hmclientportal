66a5636b50e0b0a4ee2ea760963ae9e17079a242
Josh Larson <jplhomer@gmail.com>
2013-09-30 10:42:56 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hmclientportal::Application.config.secret_key_base = '082bee471d740532923ec9edb5ed1d4ee0bdcd4a3daf794de1718616a2dd1e84702e9f7db3655d02fad71676b47349aab5c8b8f43d10590fb249c011d11c1d7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hmclientportal::Application.config.session_store :cookie_store, key: '_hmclientportal_session'
